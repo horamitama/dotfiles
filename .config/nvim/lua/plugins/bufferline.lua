1241006d3c35ece7db8301b9aaac2fb20c38222e
Seitaro Nakaue <sencha@SeitaronoMacBook-Air.local>
2026-01-04 22:10:01 +0900

    modify bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -9,7 +9,6 @@ return {
 				always_show_bufferline = true,
 				show_buffer_close_icons = false,
 				show_close_icon = false,
-				separator_style = slant,
 				diagnostics = "nvim_lsp",
 				offsets = {
 					{
