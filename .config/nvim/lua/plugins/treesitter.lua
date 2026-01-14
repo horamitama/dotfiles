b2163f6a15f218cf631faafc88c45a5920d91705
horamitama <horahora0828@gmail.com>
2026-01-14 11:54:54 +0900

    refix treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,28 +3,35 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"typescript",
-				"tsx",
-				"javascript",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"json",
-				"terraform",
-				"hcl",
-			},
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
+		local parsers = {
+			"lua",
+			"typescript",
+			"tsx",
+			"javascript",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"json",
+			"terraform",
+			"hcl",
+		}
+		require("nvim-treesitter").install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				if pcall(vim.treesitter.get_parser) then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
 	end,
 }
