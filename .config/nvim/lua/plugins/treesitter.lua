1fd50ac5e122be167f4f660a481d367fdbd2fea9
中上誠太郎 <s-nakaue@glad-cube.com>
2026-01-14 10:11:35 +0900

    fix treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,38 +3,28 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		-- パーサーのインストール
-		local parsers = {
-			"lua",
-			"typescript",
-			"tsx",
-			"javascript",
-			"yaml",
-			"html",
-			"css",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"json",
-			"terraform",
-			"hcl",
-		}
-		require("nvim-treesitter").install(parsers)
-
-		-- treesitter highlight を全ファイルタイプで有効化
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				pcall(vim.treesitter.start)
-			end,
-		})
-
-		-- treesitter indent を有効化
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				if pcall(vim.treesitter.get_parser) then
-					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				end
-			end,
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"typescript",
+				"tsx",
+				"javascript",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"json",
+				"terraform",
+				"hcl",
+			},
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
 		})
 	end,
 }
