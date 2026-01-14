663fae16f1feb941c45af3b4ecfafce757742753
horamitama <horahora0828@gmail.com>
2026-01-14 09:42:30 +0900

    fix treesitter error

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
-	-- enabled = false,
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
+	config = function()
+		-- パーサーのインストール
+		local parsers = {
 			"lua",
 			"typescript",
 			"tsx",
@@ -18,16 +18,23 @@ return {
 			"json",
 			"terraform",
 			"hcl",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
+		}
+		require("nvim-treesitter").install(parsers)
+
+		-- treesitter highlight を全ファイルタイプで有効化
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+
+		-- treesitter indent を有効化
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				if pcall(vim.treesitter.get_parser) then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
+		})
 	end,
 }
