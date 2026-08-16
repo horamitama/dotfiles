969809782f30a2e763fbb76670d8a00c7fea1782
horamitama <horahora0828@gmail.com>
2026-08-16 16:27:35 +0900

    markdownのレンダリング・プレビュープラグインを有効化
    render-markdown.nvimでバッファ内インライン描画、markdown-preview.nvimでブラウザプレビューできるようにする

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,34 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	ft = { "markdown", "quarto" },
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {
-		lsp = { enabled = true },
+	-- バッファ内インライン描画
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown", "codecompanion" },
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {
+			completions = { lsp = { enabled = true } },
+			heading = { sign = false, position = "inline" },
+			code = { sign = false, width = "block", right_pad = 2 },
+			checkbox = { enabled = true },
+		},
+	},
+
+	-- ブラウザプレビュー
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_theme = "dark" -- nord に合わせる
+			vim.g.mkdp_auto_close = 0 -- バッファを閉じてもプレビューを残す
+		end,
+		keys = {
+			{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", ft = "markdown", desc = "Markdown Preview (browser)" },
+		},
 	},
 }
